9bd971ea57db4b40a4210ca9c75fa2da6d111dbb
The Lood <unilood@gmail.com>
2022-06-14 09:05:29 +1200

    dayOfTheWeek quiz

--- src/main/kotlin/AMS.kt
+++ src/main/kotlin/AMS.kt
@@ -1,2 +1,23 @@
-class AMS {
+import java.util.*
+
+fun main(args: Array<String>) {
+    println("Hello, World!")
+    dayOfTheWeek()
+}
+
+fun dayOfTheWeek() {
+    println("What day is it today?")
+    val day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+
+    println(day)
+    println(when(day) {
+        1 -> "Monday"
+        2 -> "Tuesday"
+        3 -> "Wednesday"
+        4 -> "Thursday"
+        5 -> "Friday"
+        6 -> "Saturday"
+        7 -> "Sunday"
+        else -> "Time has stopped"
+    })
 }
