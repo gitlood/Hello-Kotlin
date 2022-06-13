f8bfbcaa2091fe5efe260cf244d57aef748a085f
The Lood <unilood@gmail.com>
2022-06-14 09:39:20 +1200

    Random Day

--- src/main/kotlin/AMS.kt
+++ src/main/kotlin/AMS.kt
@@ -1,23 +1,31 @@
 import java.util.*
+import kotlin.random.Random.Default.nextInt
 
-fun main(args: Array<String>) {
-    println("Hello, World!")
-    dayOfTheWeek()
+
+//fun main(args: Array<String>) {
+//    Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+//    println(if (args[0].toInt() < 12) "Good morning, Kotlin" else "Good night, Kotlin")
+//}
+
+fun main(args: Array<String>){
+feedTheFish()
 }
 
-fun dayOfTheWeek() {
-    println("What day is it today?")
-    val day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+fun feedTheFish(){
+val day = randomDay()
+    val food = "pellets"
+    println("Today is $day and the fish eat $food")
+}
 
-    println(day)
-    println(when(day) {
-        1 -> "Monday"
-        2 -> "Tuesday"
-        3 -> "Wednesday"
-        4 -> "Thursday"
-        5 -> "Friday"
-        6 -> "Saturday"
-        7 -> "Sunday"
-        else -> "Time has stopped"
-    })
+fun randomDay(): String {
+val week = listOf(
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+)
+    return week[Random().nextInt(7)]
 }
