745fe01046f586fcda063e6db01403065bd642fd
The Lood <unilood@gmail.com>
2022-06-15 09:59:43 +1200

    Inheritance subclass TowerTank inherits from Aquarium

--- src/main/kotlin/Aquarium/Aquarium.kt
+++ src/main/kotlin/Aquarium/Aquarium.kt
@@ -1,14 +1,16 @@
 package Aquarium
 
-class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+import java.lang.Math.PI
 
-    var volume: Int
+open class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+
+    open var volume: Int
         get() = length * width * height / 1000
         set(value) {
             height = (value * 1000) / (width * length)
         }
 
-    var water = volume * 0.9
+    open var water = volume * 0.9
 
     constructor(numberOfFish: Int) : this() {
         val water: Int = numberOfFish * 2000 // cm3
@@ -16,3 +18,12 @@ class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40)
         height = (tank / (length * width)).toInt()
     }
 }
+
+class TowerTank() : Aquarium() {
+    override var water = volume * 0.8
+
+    override var volume: Int
+    get() = (width * height * length / 1000 * PI).toInt()
+    set(value){
+        height = (value * 1000) / (width * length)}
+    }
