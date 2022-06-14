394e137335efdd1674840d057df3d987ac37a458
The Lood <unilood@gmail.com>
2022-06-14 13:59:57 +1200

    When statements

--- src/main/kotlin/AMS.kt
+++ src/main/kotlin/AMS.kt
@@ -1,36 +1,33 @@
-import java.util.*
-
 fun main(args: Array<String>) {
-    repeat(10) { feedTheFish() }
-}
-
-fun feedTheFish() {
-    val day = randomDay()
-    val food = fishFood(day)
-    println("Today is $day and the fish eat $food")
-}
-
-fun randomDay(): String {
-    val days = listOf(
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday"
-    )
-    return days[Random().nextInt(7)]
-}
-
-fun fishFood(day: String): String {
-    return when (day) {
-        "Monday" -> "flakes"
-        "Tuesday" -> "redworms"
-        "Wednesday" -> "granules"
-        "Thursday" -> "mosquitoes"
-        "Friday" -> "plankton"
-        "Saturday" -> "lettuce"
-        else -> "fasting"
+    var fortune: String
+    for (i in 1..10) {
+        fortune = getFortuneCookie(getBirthday())
+        println("\nYour fortune is: $fortune")
+        if (fortune.contains("Take it easy")) break
     }
+}
+
+fun getFortuneCookie(birthday: Int?): String {
+    val fortunes = listOf(
+        "You will have a great day!",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.",
+        "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!",
+        "Treasure your friends because they are your greatest fortune."
+    )
+    print("\nEnter your birthday: ")
+    return when (birthday) {
+        in 0..7 -> (fortunes[0])
+        in 11..15 -> (fortunes[1])
+        28 -> (fortunes[5])
+        in 28..31 -> fortunes[6]
+        else -> fortunes[birthday?.rem(fortunes.size) ?:16]
+    }
+}
+
+fun getBirthday(): Int {
+    print("/nPlease enter your birthday: ")
+    return readLine()?.toIntOrNull() ?: 1
 }
