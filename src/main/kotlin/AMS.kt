322252ce68bdfc50de75eeaf6845db767af957b8
The Lood <unilood@gmail.com>
2022-06-14 13:44:46 +1200

    Feed the Fish

--- src/main/kotlin/AMS.kt
+++ src/main/kotlin/AMS.kt
@@ -1,32 +1,36 @@
 import java.util.*
-import kotlin.random.Random.Default.nextInt
-
-
-//fun main(args: Array<String>) {
-//    Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
-//    println(if (args[0].toInt() < 12) "Good morning, Kotlin" else "Good night, Kotlin")
-//}
 
 fun main(args: Array<String>) {
-    var fortune: String
-    for (i in 1..10) {
-        fortune = getFortuneCookie()
-        print("\nYour fortune is: $fortune")
-        if (fortune.contains("Take it easy")) break
-    }
+    repeat(10) { feedTheFish() }
 }
 
-fun getFortuneCookie(): String {
-    val fortunes = listOf(
-        "You will have a great day!",
-        "Things will go well for you today.",
-        "Enjoy a wonderful day of success.",
-        "Be humble and all will turn out well.",
-        "Today is a good day for exercising restraint.",
-        "Take it easy and enjoy life!",
-        "Treasure your friends because they are your greatest fortune."
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println("Today is $day and the fish eat $food")
+}
+
+fun randomDay(): String {
+    val days = listOf(
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
     )
-    print("\nEnter your birthday: ")
-    val birthday = readLine()?.toIntOrNull() ?: 1
-    return fortunes[birthday.rem(fortunes.size)]
+    return days[Random().nextInt(7)]
+}
+
+fun fishFood(day: String): String {
+    return when (day) {
+        "Monday" -> "flakes"
+        "Tuesday" -> "redworms"
+        "Wednesday" -> "granules"
+        "Thursday" -> "mosquitoes"
+        "Friday" -> "plankton"
+        "Saturday" -> "lettuce"
+        else -> "fasting"
+    }
 }
