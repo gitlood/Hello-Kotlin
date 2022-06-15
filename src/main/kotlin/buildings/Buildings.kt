836665f5bf43c811e5e25a7c1420e879833124cf
The Lood <unilood@gmail.com>
2022-06-16 08:32:08 +1200

    Generic in and out excercise

--- src/main/kotlin/buildings/Buildings.kt
+++ src/main/kotlin/buildings/Buildings.kt
@@ -23,3 +23,14 @@ class Building<T: BaseBuildingMaterials>(val buildingMaterials: T){
 fun main(args: Array<String>) {
     Building(Wood()).build()
 }
+
+/*
+Create a generic function for type BaseBuildingMaterial and call it isSmallBuilding, which takes a Building with a building material T as an argument. If the materials needed are less than 500, print "small building", otherwise, print "large building".
+Note: For this function, IntelliJ recommends not to inline the function. Generally, when you create a generic function, follow the IDE's recommendation about inlining.
+*/
+
+fun <T : BaseBuildingMaterials> isSmallBuilding(building: Building<T>) {
+    if (building.actualMaterialsNeeded < 500) println("Small building")
+    else println("large building")
+}
+isSmallBuilding(Building(Brick()))
